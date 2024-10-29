79fc7a17a9e2a0f89c835089bb656ceff6242d93
Dawson Dohlen <d.dohlen@pm.me>
2024-10-28 18:14:03 -0700

    add department view, budgetaggregation, add department, view all roles, and exit logic and queries

--- server.js
+++ server.js
@@ -41,13 +41,58 @@ pool.query('SELECT * FROM employee', function (err, {rows}) {
     console.log(`Server running on port ${PORT}`);
   });
 
-// Inquirer 
-
+// Inquirer Input 
 var employee_tracker = function() {
     inquirer.prompt([{
        type: 'list',
        name: 'prompt',
        message: 'What would you like to do?',
-       choices: ['View all departments', 'View all roles']
-    }])
-}
+       choices: ['View all departments', 'View department budget', 'Add a new department', 'View all roles', 'Add a new role', 'View all employees', 'Add an employee', 'Update an employee role', 'Delete an employee', 'Exit']
+    }]).then((answers) => {
+        if (answers.prompt === 'View all departments') {
+            pool.query(`SELECT * FROM department`, (err,result) => {
+                if (err) throw err;
+                console.log("Showing all departments: ");
+                console.table(result);
+                employee_tracker();
+            });
+        } else if (answers.prompt === 'View department budget') {
+            pool.query(`SELECT c.name as department_name, SUM(b.salary) as combined_salaries FROM employee as a JOIN role as b ON b.id = a.role_id JOIN department as c ON c.id = b.department_id GROUP BY 1 ORDER BY 1 asc`, (err, result) => {
+                if (err) throw err;
+                console.log("Showing combined salaries by department: ");
+                console.table(result);
+                employee_tracker();
+            });
+        } else if (answers.prompt === 'Add a new department') {
+            inqurier.prompt([{
+                type: 'input',
+                name: 'department',
+                prompt: 'What is the name of the new department?',
+                validate: departmentInput => {
+                    if (departmentInput) {
+                        return true;
+                    } else {
+                        console.log('Please add a department!');
+                        return false;
+                    }
+                }
+            }]).then((answers) => {
+                pool.query(`INSERT INTO department (name) VALUES (?)`, [answers.department], (err, result) => {
+                  if (err) throw err;
+                  console.log(`Added ${answers.department} to the datbase.`) 
+                  employee_tracker(); 
+                });
+            })
+        } else if (answers.prompt === 'View all roles') {
+            pool.query(`SELECT * FROM role`, (err, result) => {
+                if (err) throw err;
+                console.log("Showing all roles: ");
+                console.table(result);
+                employee_tracker();
+            });
+        } else if (answers.prompt === 'Exit') {
+            pool.end();
+            console.log("Good Bye!");
+        }
+    })
+};
