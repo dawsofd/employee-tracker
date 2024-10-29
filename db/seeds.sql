8be8a60f5ef0d11287ff0f348d60c9b865fec165
Dawson Dohlen <d.dohlen@pm.me>
2024-10-29 16:00:07 -0700

    manually add ids to deparment, role, and employee seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,17 +4,17 @@ DO $$
   BEGIN
 
 -- Department
-INSERT INTO department (name)
-VALUES  ('Engineering'),
-        ('Analytics'),
-        ('Marketing'),
-        ('Sales'),
-        ('Finance'),
-        ('Recruiting'),
-        ('HR'),
-        ('Legal'),
-        ('Customer Support'),
-        ('Executive');
+INSERT INTO department (id, name)
+VALUES  (1, 'Engineering'),
+        (2, 'Analytics'),
+        (3, 'Marketing'),
+        (4, 'Sales'),
+        (5, 'Finance'),
+        (6, 'Recruiting'),
+        (7, 'HR'),
+        (8, 'Legal'),
+        (9, 'Customer Support'),
+        (10, 'Executive');
 
 -- Role
 INSERT INTO role (title, salary, department_id)
@@ -34,27 +34,27 @@ VALUES  ('Software Engineer', 185000, 1),
         ('General Counsel', 175000, 8),
         ('Customer Support Specialist', 65000, 9),
         ('Chief Executive Officer', 300000, 10),
-        ('Chief Operations Officer', 275000, 11);
+        ('Chief Operations Officer', 275000, 10);
 
 -- Employee
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ('Abe', 'Smith', 1, 3),
-        ('Barbara', 'Johnson', 2, 3),
-        ('Ricardo', 'Garcia', 3, 10),
-        ('Elizabeth', 'Perry', 4, 3),
-        ('Natasha', 'Gopaul', 5, 3),
-        ('Shea', 'Adams', 6, 7),
-        ('Robert', 'Ansel', 7, 10),
-        ('Jamie', 'Collins', 8, 7),
-        ('Peter', 'Shea', 9, 11),
-        ('Adam', 'Robertson', 10, 11),
-        ('Karim', 'Rashid', 11, 10),
-        ('Sang', 'Graham', 12, 17),
-        ('Connie', 'Sherman', 13, 17),
-        ('Clint', 'Carter', 14, 17),
-        ('Nathaniel', 'Levy', 15, 17),
-        ('Ivory', 'Leach', 16, NULL),
-        ('Tabatha', 'Gates', 17, 16);
+VALUES  ('Abe', 'Smith', 101, 1003),
+        ('Barbara', 'Johnson', 102, 1003),
+        ('Ricardo', 'Garcia', 103, 1010),
+        ('Elizabeth', 'Perry', 104, 1003),
+        ('Natasha', 'Gopaul', 105, 1003),
+        ('Shea', 'Adams', 106, 1007),
+        ('Robert', 'Ansel', 107, 1010),
+        ('Jamie', 'Collins', 108, 1007),
+        ('Peter', 'Shea', 109, 1011),
+        ('Adam', 'Robertson', 110, 1011),
+        ('Karim', 'Rashid', 111, 1010),
+        ('Sang', 'Graham', 112, 1017),
+        ('Connie', 'Sherman', 113, 1017),
+        ('Clint', 'Carter', 114, 1017),
+        ('Nathaniel', 'Levy', 115, 1017),
+        ('Ivory', 'Leach', 116, NULL),
+        ('Tabatha', 'Gates', 117, 1016);
 
 EXCEPTION
     WHEN OTHERS THEN
